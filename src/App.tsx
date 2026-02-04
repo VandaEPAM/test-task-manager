fc8958ea1e963271ab63264bbd8c77c66156473f
Vanda_Ponikarova <vanda_ponikarova@epam.com>
2026-02-04 10:36:00 +0100

    Add base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
+import { Box, Container, MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
+import { TaskForm } from './components/task-form';
+import { TaskList } from "./components/task-list";
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  return (
+    <MantineProvider theme={theme}>
+      <Container strategy="grid" size='md' bg='var(--mantine-color-blue-light)'>
+        <Box><TaskForm /></Box>
+        <Box><TaskList /></Box>
+      </Container>
+    </MantineProvider>
+  );
 }
