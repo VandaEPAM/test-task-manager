1f3a3613bba8bfd3524f1d85c9a6b3a43659a058
Vanda_Ponikarova <vanda_ponikarova@epam.com>
2026-02-04 10:59:10 +0100

    Add base Add Task form

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,29 @@ import { Box, Container, MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
 import { TaskForm } from './components/task-form';
 import { TaskList } from "./components/task-list";
+import { useState } from "react";
+
+const toTask = (values: any) => ({
+  id: Date.now(), // Simple ID generation acceptable
+  title: values.title,
+  priority: values.priority, // "high" | "medium" | "low"
+  completed: false,
+  createdAt: new Date().toISOString()
+});
 
 export default function App() {
+  const [tasks, setTasks] = useState<any[]>([]);
+
+  const handleSubmitTask = (values: any) => {
+    setTasks((prev) => [...prev, toTask(values) ])
+  };
+
+  console.log(tasks);
+
   return (
     <MantineProvider theme={theme}>
       <Container strategy="grid" size='md' bg='var(--mantine-color-blue-light)'>
-        <Box><TaskForm /></Box>
+        <Box><TaskForm onSubmit={handleSubmitTask} /></Box>
         <Box><TaskList /></Box>
       </Container>
     </MantineProvider>
--- src/components/task-form/TaskForm.tsx
+++ src/components/task-form/TaskForm.tsx
@@ -1,3 +1,46 @@
-export const TaskForm = () => {
-    return 'My Task Form';
+import { Button, Group, Select, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+
+const TASK_PRIORITY_LIST = ['High', 'Medium', 'Low'];
+
+export const TaskForm = ({ onSubmit }: { onSubmit: (values: any) => void }) => {
+    const form = useForm({
+        mode: 'uncontrolled',
+        initialValues: {
+            title: '',
+            priority: '',
+        },
+        // TODO: check how to validate properly
+        validate: {
+            title: (value) => (value.length < 5 ? 'Title must have at least 5 letters' : null),
+            priority: (value) => (TASK_PRIORITY_LIST.includes(value) ? null : 'Priority should be choosen'),
+        },
+    });
+
+    return (
+        // TODO: reset form after submit
+        <form onSubmit={form.onSubmit(onSubmit)}>
+            <TextInput
+                required
+                withAsterisk
+                label="Title"
+                placeholder="Give a name to your task..."
+                key={form.key('title')}
+                {...form.getInputProps('title')}
+            />
+
+            <Select
+                required
+                label="Choose the task priority"
+                placeholder="Pick value"
+                key={form.key('priority')}
+                data={TASK_PRIORITY_LIST}
+                {...form.getInputProps('priority')}
+            />
+
+            <Group justify="flex-end" mt="md">
+                <Button type="submit">Submit</Button>
+            </Group>
+        </form>
+    );
 };
